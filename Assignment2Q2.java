28049ca6d99e7b893579c5f6bfa5944ffc266bae
Shishir Aryal <67183366+csr88@users.noreply.github.com>
2021-05-17 21:34:40 +0545

    Add files via upload

--- Assignment2Q2.java
+++ Assignment2Q2.java
@@ -12,46 +12,43 @@ public class Assignment2Q2 {
         for (int i=0;i<5;i++){
             arr[i] = sc.nextInt();
             total += arr[i];
+
         }
         System.out.println("");
 
         System.out.println("Total marks obtained: " + total);
         double percentage = ((total/500)*100);
-        
-        //use float, it will automatically round up the value
+
         DecimalFormat value = new DecimalFormat("#.#");
         //Double.parseDouble sends the string as double
         percentage = Double.parseDouble(value.format(percentage));
 
         System.out.println("percentage score: " + percentage + "%");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the name of student: ");
-        String name = scanner.nextLine();
-
-
 
         PrintWriter outF = new PrintWriter("/home/shishir/IdeaProjects/Shishir/Javalab 10 qsns/src/score.txt");
         outF.printf("########### RESULT ###########\n");
-        outF.printf("Name: " + name + "\n");
+        String division;
 
         if (percentage<29.9){
-            System.out.println("Division: FAIL");
-            outF.printf("Division: fail");
+            division = "FAIL";
         }
         else if (percentage >=30 && percentage<60){
-            System.out.println("Division: SECOND");
-            outF.printf("Division: second ");
+            division = "SECOND";
 
         }
         else if (percentage >=60 && percentage<80){
-            System.out.println("Division: FIRST");
-            outF.printf("Division: first");
-
+            division = "FIRST";
         }
         else{
-            System.out.println("Division: DISTINCTION");
-            outF.printf("Division: distinction");
+            division = "DISTINCTION";
         }
+
+        System.out.println("DIVSION: " + division);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of student: ");
+        String name = scanner.nextLine();
+        outF.printf("Name: " + name + "\n");
+        outF.printf("Division: " + division.toLowerCase());
 
         outF.close();
 
